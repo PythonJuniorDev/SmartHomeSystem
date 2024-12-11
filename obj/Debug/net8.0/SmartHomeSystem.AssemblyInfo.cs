9d400d68aa3b1cbd7d8586f6946329fff7e832a9
Miriam Amine <m.amine@aol.nl>
2024-12-11 17:27:15 +0100

    Initial commit: Added smart home system project

--- obj/Debug/net8.0/SmartHomeSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartHomeSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHomeSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd82eba383182fbf8f9d1a702e2bb93df992daf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHomeSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHomeSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
